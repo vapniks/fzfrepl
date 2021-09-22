08aeef0b3482313a66cff919ba63de8053be0b65
Joe Bloggs <vapniks@yahoo.com>
2021-09-22 13:01:09 +0100

    Set i1 & i2 to 0 before while loop

--- fzfrepl.sh
+++ fzfrepl.sh
@@ -153,7 +153,7 @@ local prompt="${${cmd//\{q\}}:0:15} ${${${${cmd//\{q\}}:15}:-}:+... }"
 
 # Fix header to fit screen
 local header1="${colors[green]}${FZFREPL_HEADER:-C-g=quit:C-j=finish:C-t=toggle preview window:RET=copy selection to prompt:M-w=copy prompt to clipboard:C-v=view input:M-v=view output:M-1/2/3=change selections:M-h=show help:C-h=show more help}${colors[reset]}"
-local header2 i1 i2 ncols=$((COLUMNS-5))
+local header2 i1=0 i2=0 ncols=$((COLUMNS-5))
 until ((i2>${#header1})); do
     i2=${${header1[${i1:-0},((i1+ncols))]}[(I):]}
     header2+="${header1[$i1,((i2-1))]}
@@ -170,6 +170,7 @@ FZF_DEFAULT_OPTS+=" --bind 'enter:replace-query,ctrl-j:accept,ctrl-t:toggle-prev
 local -a qry
 IFS="
 "
+
 qry=($(cat "${FZFREPL_HISTORY}" |\
 	   fzf --query="$default_query" --sync --ansi --print-query \
 	       ${FZFREPL_HISTORY:+--history=$FZFREPL_HISTORY} \
